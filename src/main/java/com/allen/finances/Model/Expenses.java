37c279f625f0e4d8ed8ce921b08cadf2c77bcee9
Allen Lucke <allenlucke@gmail.com>
2020-07-09 17:35:22 -0500

    fixed spring-boot data import issue, added getters, setters and DB annotations to Model.Expenses

--- src/main/java/com/allen/finances/Model/Expenses.java
+++ src/main/java/com/allen/finances/Model/Expenses.java
@@ -21,4 +21,83 @@ public class Expenses {
         this.amount_due = amount_due;
     }
     
+    public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Boolean getPaid() {
+		return paid;
+	}
+
+	public void setPaid(Boolean paid) {
+		this.paid = paid;
+	}
+
+	public Timestamp getDue_by() {
+		return due_by;
+	}
+
+	public void setDue_by(Timestamp due_by) {
+		this.due_by = due_by;
+	}
+
+	public Timestamp getPaid_on() {
+		return paid_on;
+	}
+
+	public void setPaid_on(Timestamp paid_on) {
+		this.paid_on = paid_on;
+	}
+
+	public Boolean getRecurring() {
+		return recurring;
+	}
+
+	public void setRecurring(Boolean recurring) {
+		this.recurring = recurring;
+	}
+
+	public Long getAmount_due() {
+		return amount_due;
+	}
+
+	public void setAmount_due(Long amount_due) {
+		this.amount_due = amount_due;
+	}
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "paid")
+    private Boolean paid;
+    
+    @Column(name = "due_by")
+    private Timestamp due_by;
+    
+    @Column(name = "paid_on")
+    private Timestamp paid_on;
+    
+    @Column(name = "recurring")
+    private Boolean recurring;
+    
+    @Column(name = "amount_due")
+    private Long amount_due;
+    
+    
 }
