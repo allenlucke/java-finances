0aec5ca30d624b1e53006945dd802093b5b98abb
Allen Lucke <allenlucke@gmail.com>
2020-07-09 17:34:42 -0500

    fixed spring-boot data import issue, added getters, setters and DB annotations to Model.Expenses

--- src/main/java/com/allen/finances/Repository/Repository.java
+++ src/main/java/com/allen/finances/Repository/Repository.java
@@ -1,4 +1,4 @@
-package com.allen.finances.Repository
+package com.allen.finances.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.allen.finances.Model.Expenses;
